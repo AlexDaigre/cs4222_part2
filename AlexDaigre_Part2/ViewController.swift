ee2e482f21f1271c999d5140f206259fe071ec0c
Alex Daigre <alexdaigre@gmail.com>
2018-11-30 13:14:01 -0600

    Initial Commit

---
+++ AlexDaigre_Part2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlexDaigre_Part2
+//
+//  Created by Alexander Daigre on 11/30/18.
+//  Copyright © 2018 Alexander Daigre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
